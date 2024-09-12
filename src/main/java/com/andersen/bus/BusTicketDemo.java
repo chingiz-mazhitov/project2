9f1cd0b0832d094e6ccf0e16925d9966d1a737d1
Chingiz Mazhitov <chingiz.mazhitov56@gmail.com>
2024-09-12 17:10:06 +0800

    create CustomJsonReader class for reading json file and returning valid BusTickets

--- src/main/java/com/andersen/bus/BusTicketDemo.java
+++ src/main/java/com/andersen/bus/BusTicketDemo.java
@@ -17,44 +17,12 @@ public class BusTicketDemo {
 
 		Path path = Path.of("src/main/java/com/andersen/bus/info.json");
 
-		List<BusTicket> validTickets = readJSONFile(path);
+		List<BusTicket> validTickets = CustomJsonReader.getValidBusTickets(path);
 
 		System.out.println("-".repeat(80));
 		for (BusTicket validTicket : validTickets) {
 			System.out.println(validTicket);
 		}
 
-
-	}
-
-	public static List<BusTicket> readJSONFile(Path path) {
-		try {
-			String content = new String(Files.readAllBytes(path));
-			JSONObject jsonObject = new JSONObject(content);
-			JSONArray jsonArray = jsonObject.getJSONArray("tickets");
-
-			BusTicketValidator validator = new BusTicketValidator();
-			List<BusTicket> ticketsToPass = new ArrayList<>();
-			BusTicket busTicket;
-
-			for (int i = 0; i < jsonArray.length(); i++) {
-				var element = jsonArray.getJSONObject(i);
-				String ticketClass = element.optString("ticketClass", null);
-				String ticketType = element.optString("ticketType", null);
-				String startDate = element.optString("startDate", null);
-				BigDecimal price = element.optBigDecimal("price", null);
-
-				TicketClass ticketClassConverted = (ticketClass != null) ? TicketClass.valueOf(ticketClass) : null;
-				TicketType ticketTypeConverted = (ticketType != null) ? TicketType.valueOf(ticketType) : null;
-				LocalDate startDateConverted = (startDate == null || startDate.isEmpty()) ? null : LocalDate.parse(startDate);
-
-				busTicket = new BusTicket(ticketClassConverted, ticketTypeConverted, startDateConverted, price);
-				ticketsToPass.add(busTicket);
-			}
-
-			return validator.isValid(ticketsToPass);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
 	}
 }
